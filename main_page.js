46995ddb222ccefde94151a25a17f88fe89e87ea
NitroCoder272 <82658172+NitroCoder272@users.noreply.github.com>
2021-06-01 19:17:35 +0530

    Add files via upload

---
+++ main_page.js
@@ -0,0 +1,15 @@
+function flappy(){
+    window.location="flappy_game.html";
+}
+
+function dance(){
+    window.location="dance_page.html";
+}
+
+function quiz(){
+    window.location="quiz_game.html";
+}
+
+function bottle(){
+    window.location="bottle_game.html";
+}
